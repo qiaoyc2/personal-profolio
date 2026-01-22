5645bf211f508743591bc7c6f29ecf8493c5a0e0
Katharina Cheng <qiaoyc2@uw.edu>
2026-01-21 16:48:54 -0800

    get all the info updated

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const yearEl = document.getElementById("year");
 const lastUpdatedEl = document.getElementById("lastUpdated");
 
 if (yearEl) yearEl.textContent = new Date().getFullYear();
-if (lastUpdatedEl) lastUpdatedEl.textContent = new Date().toLocaleDateString();
+if (lastUpdatedEl) lastUpdatedEl.textContent = new Date().toLocaleDateString(undefined, { year:"numeric", month:"short", day:"numeric" });
+
