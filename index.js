b1dcb95b300b4c59906c55de26e7584417f9cb9d
Katharina Cheng <qiaoyc2@uw.edu>
2026-01-21 16:19:21 -0800

    initial version of profolio

---
+++ index.js
@@ -0,0 +1,6 @@
+// Footer dates
+const yearEl = document.getElementById("year");
+const lastUpdatedEl = document.getElementById("lastUpdated");
+
+if (yearEl) yearEl.textContent = new Date().getFullYear();
+if (lastUpdatedEl) lastUpdatedEl.textContent = new Date().toLocaleDateString();
